e8eb69073e05e84fe6c0cee7ac5cc9f31539988b
batyrbek2k07 <colponbevbaatyrbek@gmail.com>
2024-05-18 13:51:16 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { log } from "console";
+import "./App.css";
+import { GameContainer } from "./containers/Game/Game";
+
+function App() {
+  return <GameContainer />;
+  
+}
+
+export default App;
